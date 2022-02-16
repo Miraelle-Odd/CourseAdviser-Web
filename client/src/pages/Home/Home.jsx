97b2333326de57c368fad80bc5e744ab1adc0ca2
NA <19521191@gm.uit.edu.vn>
2022-02-16 10:14:20 +0700

    test axios

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+import axios from 'axios'
 import './Home.css'
 
 import DraftCom from '../../components/DraftComponent/DraftCom'
 
 const Home = props => {
+    const [listOfTest, setListOfTest] = useState([])
+    useEffect(()=> {
+        axios.get("http://localhost:8080/tests").then((response)=> {
+            setListOfTest(response.data)
+        })
+    }, [])
     return (
         <div className='home'>
             <div>Đây là Homepage</div>
@@ -12,6 +19,15 @@ const Home = props => {
                 content="Đây là một component của homepage. Thuộc tính gồm icon, title, content"
             >                
             </DraftCom>
+            <h2>Dưới đây là test get từ db</h2>
+            {
+                listOfTest.map((value, key)=>{
+                    return <DraftCom
+                    title={value.title}
+                    content={value.text}
+                    ></DraftCom>
+                })
+            }            
         </div>
     )
 }
