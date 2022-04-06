27470b3f41634a30f235d2144c1c6e5ebee72c60
Nqunhth <80096230+Nqunhth@users.noreply.github.com>
2022-04-06 19:00:00 +0700

    Full model-database/ Remove test model

--- server/routes/aa.js
+++
@@ -1,4 +0,0 @@
-// const Accounts = require("../models/Accounts")
-// const Personal_Infos = require("../models/Personal_Infos")
-
-// Personal_Infos.belongsTo(Accounts, {as: 'Accounts', foreignKey: 'account_id'});
