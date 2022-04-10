c83d984e9f717216bf1bfa675ae7951797e9864e
NA <19521191@gm.uit.edu.vn>
2022-04-10 16:54:37 +0700

    remove trash

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CookiesProvider } from 'react-cookie';
 
 import './fonts/Roboto-Thin.ttf'
 import './fonts/Roboto-Light.ttf'
