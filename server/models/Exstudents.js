09c52de7c1f11ec7c360c9fd59fac0001c633cd8
NA <19521191@gm.uit.edu.vn>
2022-05-08 15:45:47 +0700

    update model exstudent for shorter data

--- server/models/Exstudents.js
+++ server/models/Exstudents.js
@@ -14,43 +14,42 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        exst_achieve:{
+        exst_achieve: {
             type: DataTypes.STRING(100),
             allowNull: false
         },
-        exst_quote:{
+        exst_quote: {
             type: DataTypes.STRING(100),
             allowNull: false
         }
     });
-    const list = [
-        {
-            exst_name:'Hồ Quang Trí',   
-            exst_img:'https://i.imgur.com/6oVgYrP.jpg',
-            exst_achieve:'IELTS total from 5.0 to 7.5',
-            exst_quote:'Cám ơn cô B đã luôn tạo động lực và khích lệ em ở những khoảnh khắc em dường như đã  bỏ cuộc'
+    const list = [{
+            exst_name: 'Hồ Quang Trí',
+            exst_img: 'https://i.imgur.com/6oVgYrP.jpg',
+            exst_achieve: 'IELTS 5.0 to 7.5',
+            exst_quote: 'Cám ơn cô B đã luôn tạo động lực và khích lệ em ở những khoảnh khắc em dường như đã  bỏ cuộc'
         },
         {
-            exst_name:'Lương Văn Thiện', 
-            exst_img:'https://i.imgur.com/926qgu4.jpg',  
-            exst_achieve:'IELTS total from 6.5 to 8.0',
-            exst_quote:'Người thành công có lối đi riêng. Trung tâm là một điểm mốc trên lối đi đó của em.\r\n'
+            exst_name: 'Lương Văn Thiện',
+            exst_img: 'https://i.imgur.com/926qgu4.jpg',
+            exst_achieve: 'IELTS 6.5 to 8.0',
+            exst_quote: 'Người thành công có lối đi riêng. Trung tâm là một điểm mốc trên lối đi đó của em.\r\n'
         },
         {
-            exst_name:'Phan Tiểu Linh', 
-            exst_img:'https://i.imgur.com/y7NhfrY.jpg',
-            exst_achieve:'IELTS total from 6.0 to 8.0', 
+            exst_name: 'Phan Tiểu Linh',
+            exst_img: 'https://i.imgur.com/y7NhfrY.jpg',
+            exst_achieve: 'IELTS 6.0 to 8.0',
             exst_quote: 'Em cám ơn trung tâm và thầy A đã luôn kiên nhẫn khai phá năng lực của em'
         },
         {
-            exst_name:'Lê Minh Thông', 
-            exst_img:'https://i.imgur.com/VqCzq45.jpg',
-            exst_achieve: 'IELTS total from unknow to 8.0',
+            exst_name: 'Lê Minh Thông',
+            exst_img: 'https://i.imgur.com/VqCzq45.jpg',
+            exst_achieve: 'IELTS scratch to 8.0',
             exst_quote: 'Trung tâm đã hỗ trợ cho em từ ngày đầu em đi học không biết gì. Em chân thành cảm ơn.'
         }
     ]
     list.forEach(element => {
-        Exstudents.sync().then(async function () {
+        Exstudents.sync().then(async function() {
             await Exstudents.findOrCreate({ where: element })
         })
     });
